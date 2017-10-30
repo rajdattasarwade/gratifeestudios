dfc507a4c4270708f7963b99f1ec7a6afea613fb
Rajdatta Sarwade <rajdattatestemail@gmail.com>
2017-10-30 17:49:21 +0530

    Index file commit

--- wp-content/themes/vertex/index.php
+++ wp-content/themes/vertex/index.php
@@ -1,57 +1,100 @@
 <?php
 get_header();
 ?>
-	<div class="content">
-		<div class="container">
-			<div class="post_content">
-			<?php if ( have_posts() ) : ?>
-				 <div class="blog">
-						<div class="blog-posts">
-							<?php
-								while ( have_posts() ) : the_post();
-								?>
-									<div class="blog-post-box <?php echo (is_sticky() ? 'sticky-post': ''); ?>">
-										<div class="blog-post-feature">
-										<?php
+<div class="home-slider">
+	<div id="slider" class="sl-slider-wrapper">
+		<div class="sl-slider">
+		
+			<div class="sl-slide bg-1" data-orientation="horizontal" data-slice1-rotation="-25" data-slice2-rotation="-25" data-slice1-scale="2" data-slice2-scale="2">
+				<div class="sl-slide-inner">
+					<div class="bg-img" style="background-image: url(http://dessign.net/vertex/wp-content/uploads/2016/01/snow-landscape-mountains-nature.jpg); "><a href="http://dessign.net/vertex/amazing-landscape-photography/"></a></div>
+				</div>
+			</div>
+			
+			<div class="sl-slide bg-2" data-orientation="vertical" data-slice1-rotation="-25" data-slice2-rotation="-25" data-slice1-scale="2" data-slice2-scale="2">
+				<div class="sl-slide-inner">
+					<div class="bg-img" style="background-image: url(http://dessign.net/vertex/wp-content/uploads/2016/01/pexels-photo-14287.jpeg); ">
+						<a href="http://dessign.net/vertex/digital-photography-post/"></a>
+					</div>
+				</div>
+			</div>
 
-							 				if ( 'video' == get_post_format( get_the_ID() ) ) :
+		</div>
 
-							 					echo vertex_get_video( get_the_ID() );
+		<nav id="nav-arrows" class="nav-arrows">
+			<span class="nav-arrow-prev">Previous</span>
+			<span class="nav-arrow-next">Next</span>
+		</nav>
 
-							 				else :
+	</div>
+</div>	
 
-							 					$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array(600,600) ); 
-												echo '<div class="blog-post-image">
-															<a href="'.esc_url( get_permalink() ).'" style="background-image: url('. esc_url( $thumbnail[0] ).')"></a>
-														</div>';
-											endif;
-												
-										?>
-										</div>
-										<div class="blog-post-info">
-											<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-											<div class="blog-post-meta"> <?php the_time(get_option('date_format')); ?> | <a href="<?php comments_link(); ?>"><?php  comments_number( __('0 Comments','vertex'), __(' 1 Comment','vertex'), '% '.__('Comments','vertex') ); ?></a></div>
-											<div class="blog-post-excerpt">
-												<p><?php echo wp_trim_words( get_the_excerpt(), 25, '...' ) ?></p>
-											</div>
-										</div>
-									</div>
-								<?php
-									endwhile;
-								?>
-						</div><!-- blog-posts -->
+<div class="feature-text-area">
+	<div class="container">
+		<h3>Vertex - Free Creative Responsive WordPress Theme</h3>
+	</div><!-- container -->
+</div>
 
-						<div class="blog-pagination">
-							<?php
-								the_posts_pagination();
-							?>
-						</div>
-				</div><!-- blog -->
-			<?php else: echo '<p>'.__('Sorry, no blog posts found. Please create a post and assign it in "blog" category.','vertex').'</p>'; endif; ?>
+<div class="home-portfolio">
+	<div class="container">
+		<div class="portfolio-box">
+			<div class="port-image">
+				<a href="http://dessign.net/vertex/amazing-sea-image/" style="background-image: url(http://dessign.net/vertex/wp-content/uploads/2016/01/landscape-nature-sky-man-1.jpeg)">
+					
+				</a>			
+			</div>
+			<div class="port-body">
+				<h3><a href="http://dessign.net/vertex/amazing-sea-image/">Amazing Sea Image</a></h3>
+				<div class="port-cats"> <a href="http://dessign.net/vertex/category/blog/" rel="category tag">blog</a>, <a href="http://dessign.net/vertex/category/landscape/" rel="category tag">Landscape</a>, <a href="http://dessign.net/vertex/category/nature/" rel="category tag">Nature</a>, <a href="http://dessign.net/vertex/category/people/" rel="category tag">People</a></div>
 			</div>
 		</div>
-	</div>
-	
+		<div class="portfolio-box">
+			<div class="port-image">
+				<a href="http://dessign.net/vertex/digital-photographer/" style="background-image: url(http://dessign.net/vertex/wp-content/uploads/2016/01/night.jpg)">
+					
+				</a>			
+			</div>
+			<div class="port-body">
+				<h3><a href="http://dessign.net/vertex/digital-photographer/">Digital Photographer</a></h3>
+				<div class="port-cats"> <a href="http://dessign.net/vertex/category/landscape/" rel="category tag">Landscape</a>, <a href="http://dessign.net/vertex/category/people/" rel="category tag">People</a>, <a href="http://dessign.net/vertex/category/photography/" rel="category tag">Photography</a></div>
+			</div>
+		</div>
+		<div class="portfolio-box">
+			<div class="port-image">
+				<a href="http://dessign.net/vertex/people-and-landscape/" style="background-image: url(http://dessign.net/vertex/wp-content/uploads/2016/01/pexels-photovvv.jpg)"></a>			</div>
+			<div class="port-body">
+				<h3><a href="http://dessign.net/vertex/people-and-landscape/">People and Landscape</a></h3>
+				<div class="port-cats"> <a href="http://dessign.net/vertex/category/landscape/" rel="category tag">Landscape</a>, <a href="http://dessign.net/vertex/category/photography/" rel="category tag">Photography</a>, <a href="http://dessign.net/vertex/category/visual-images/" rel="category tag">Visual Images</a></div>
+			</div>
+		</div>
+		<div class="portfolio-box">
+			<div class="port-image">
+				<a href="http://dessign.net/vertex/nature-and-photography/" style="background-image: url(http://dessign.net/vertex/wp-content/uploads/2016/01/pexels-photow.jpg)"></a>			</div>
+			<div class="port-body">
+				<h3><a href="http://dessign.net/vertex/nature-and-photography/">Nature and Photography</a></h3>
+				<div class="port-cats"> <a href="http://dessign.net/vertex/category/landscape/" rel="category tag">Landscape</a>, <a href="http://dessign.net/vertex/category/nature/" rel="category tag">Nature</a>, <a href="http://dessign.net/vertex/category/people/" rel="category tag">People</a>, <a href="http://dessign.net/vertex/category/photography/" rel="category tag">Photography</a></div>
+			</div>
+		</div>
+		<div class="portfolio-box">
+			<div class="port-image">
+				<a href="http://dessign.net/vertex/photography-landscape/" style="background-image: url(http://dessign.net/vertex/wp-content/uploads/2016/01/pexels-photovddd.jpg)"></a>			</div>
+			<div class="port-body">
+				<h3><a href="http://dessign.net/vertex/photography-landscape/">Photography Landscape</a></h3>
+				<div class="port-cats"> <a href="http://dessign.net/vertex/category/landscape/" rel="category tag">Landscape</a>, <a href="http://dessign.net/vertex/category/nature/" rel="category tag">Nature</a>, <a href="http://dessign.net/vertex/category/photography/" rel="category tag">Photography</a>, <a href="http://dessign.net/vertex/category/visual-images/" rel="category tag">Visual Images</a></div>
+			</div>
+		</div>
+		<div class="portfolio-box">
+			<div class="port-image">
+				<a href="http://dessign.net/vertex/modern-photography-post/" style="background-image: url(http://dessign.net/vertex/wp-content/uploads/2016/01/pexels-photoas44.jpg)"></a>			</div>
+			<div class="port-body">
+				<h3><a href="http://dessign.net/vertex/modern-photography-post/">Modern Photography Post</a></h3>
+				<div class="port-cats"> <a href="http://dessign.net/vertex/category/landscape/" rel="category tag">Landscape</a>, <a href="http://dessign.net/vertex/category/nature/" rel="category tag">Nature</a>, <a href="http://dessign.net/vertex/category/photography/" rel="category tag">Photography</a></div>
+			</div>
+		</div>
+			<div class="clear"></div>
+	</div><!-- container -->
+</div>
+
 <?php
 	get_footer();
 ?>
